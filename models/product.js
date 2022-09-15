104fde3afa4200ed07569553eccfc3afc01062b1
Prashant <prashantstha1110@gmail.com>
2022-09-15 10:04:02 +0545

    changes product.js

--- models/product.js
+++ models/product.js
@@ -8,10 +8,9 @@ var schema = new Schema ({
     description : {type: String, required:true},
     price : {type: Number, required:true},
     category : {type: [String], required:true},
-    rating : {type:Number,required:true}
+    rating : {type:Number, required:true}
 });
 
 const Product = mongoose.model('Product',schema);
 module.exports = Product;
 
-//module.exports = mongoose.model('product',schema);
