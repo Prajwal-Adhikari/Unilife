5644c3bb6fad6b0b5fe7498520c82671f4b29e03
Shishir Tamrakar <stamrakar545@gmail.com>
2022-10-04 13:43:37 +0545

    integrated product pagination,search,sort,filter api

--- models/product.js
+++ models/product.js
@@ -1,15 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var schema = new Schema ({
+var productSchema = new Schema ({
     imagepath : {type : String, required : true},
     productby : {type : String, required:true},
     title : {type: String, required:true},
     description : {type: String, required:true},
     price : {type: Number, required:true},
-    category : {type: String,value:[String], required:true},
+    category : {type: [String], required:true},
 });
 
-const Product = mongoose.model('Product',schema);
-module.exports = Product;
+// const Product = mongoose.model('Product',schema);
+// module.exports = Product;
 
+module.exports = mongoose.model("product", productSchema)
---
+++ routes/api/searchproduct.js
@@ -0,0 +1,74 @@
+const Router = require("express").Router();
+const Product = require("../../models/product");
+const products = require("../../config/products.json");
+
+Router.get("/products", async(req, res) => {
+    try{
+        const page = parseInt(req.query.page) - 1 || 0;
+        const limit = parseInt(req.query.limit) || 5;
+        const search = req.query.search || "";
+        let sort = req.query.sort || "price";
+        let category = req.query.category || "All";
+
+        const categoryOptions = [
+            "Furniture",
+            "Education",
+            "Utensil",
+            "Others"
+        ]
+
+        category === "All"
+            ? (category = [...categoryOptions])
+            : (category = req.query.category.split(","));
+        
+        req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+        let sortBy = {};
+        if(sort[1]) {
+            sortBy[sort[0]] = sort[1];
+        } else {
+            sortBy[sort[0]] = "asc";
+        }
+
+        const products = await Product.find({title: {$regex: search, $options: "i"}})
+            .where("category")
+            .in([...category])
+            .sort(sortBy)
+            .skip(page * limit)
+            .limit(limit);
+
+        const total = await Product.countDocuments({
+            category: {$in: [...category]},
+            title: { $regex: search, $options: "i"},
+        });
+
+        const response = {
+            error: false,
+            total,
+            page: page + 1,
+            limit,
+            categories: categoryOptions,
+            products,
+        }
+
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: true, message:"Internal Server Error"});
+    }
+});
+
+// const insertProducts = async() => {
+//     try {
+//         const docs = await Product.insertMany(products);
+//         return Promise.resolve(docs);
+//     } catch (err) {
+//         return Promise.reject(err);
+//     }
+// }
+
+// insertProducts()
+//     .then((docs) => console.log(docs))
+//     .catch((err) => console.log(err))
+
+module.exports = Router;
