81990b335b76f4acd582a7b5c04b851444632567
Prajwal-Adhikari <prazolppp@gmail.com>
2022-11-03 22:47:19 +0545

    added footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,49 +1,63 @@
 import React from "react";
-import './Footer.css'
+import './Footer.css';
+import logo from '../../static/logo.png';
 import { Component } from "react";
-import {FaTwitter,FaInstagram,FaFacebook} from "react-icons/fa"
+import {FaTwitter,FaInstagram,FaFacebook,FaMapMarkerAlt, FaPhone, FaEnvelope} from "react-icons/fa"
+// import { Facebook, Instagram, Twitter, Room, Phone, MailOutline } from "@material-ui/icons";
 
 class Footer extends Component{
     render(){
         return(
-            <section>
-                <footer id="footer">
-                    <div className="Footer_Container grid grid-three-column">
-                        <div className="footer-about">
-                            <h3>Unilife</h3>
-                            <p>Make your University life easier here with us. Search,find and complete your end goal. Add your product and sell them easily.</p>
-                        </div>
-
-                        <div className="footer-social">
-                            <h3>Follow Us </h3>
-                            <div clasName="footer-social --icons">
-                                <div>
-                                <a href="#" target="_blank">
-                                    <FaTwitter className="icons"/>
-                                </a>
-                                </div>
-                                <div>
-                                <a href="#" target="_blank">
-                                    <FaFacebook className="icons"/>
-                                </a>
-                                </div>
-                                <div>
-                                    <a href="#" target="_blank">
-                                    <FaInstagram className="icons"/>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="footer-contact">
-                            <h3>Contact Us</h3>
-                            <h3>+977 980197869987</h3>
-                            <h3>+01 2556456</h3>
-                        </div>
-
+            <div className="container-footer">
+          
+                <div className="left">
+                    
+                    <div className="title">
+                    <img src={logo} alt="Logo" className="logo-image"/>
                     </div>
-                </footer>
-            </section>
+                    <div className="description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam natus doloribus non eum dignissimos ullam aspernatur obcaecati porro deleniti aliquid incidunt perspiciatis consequatur earum recusandae officiis officia, eligendi tenetur minima, dolore amet dolorum enim velit.
+                    </div>
+                    <div className="socialContainer">
+
+                       
+                        <div className="socialIcon" id="facebook">
+                            <FaFacebook /> 
+                        </div>
+                        <div className="socialIcon" id="instagram">
+                            <FaInstagram /> 
+                        </div>
+                        <div className="socialIcon" id="twitter">
+                            <FaTwitter /> 
+                        </div>
+                    </div>
+                </div>
+
+                <div className="center">
+                    <div className="title">USEFUL LINKS</div>
+                        <div className="list">
+                      
+                            <div className="listItem">Home</div>
+                            <div className="listItem">About Us</div>
+                            <div className="listItem">Hostels</div>
+                            <div className="listItem">Rooms</div>
+                            <div className="listItem">Shop</div>
+                            <div className="listItem">Cart</div>
+                            <div className="listItem">Terms</div>
+                        </div>
+
+
+                </div>
+                <div className="right">
+                   <div className="title">CONTACTS</div>
+                    <div className="contactItem">
+                     <FaMapMarkerAlt className="fa-icons"/> 28 Kilo, Banepa-4, Dhulikhel</div>
+                    <div className="contactItem">
+                       <FaPhone className="fa-icons"/> +977 999 888 7771</div>
+                    <div className="contactItem">
+                       <FaEnvelope className="fa-icons"/> teamunilife@gmail.com</div>
+                </div>
+            </div>
         )
     }
         
