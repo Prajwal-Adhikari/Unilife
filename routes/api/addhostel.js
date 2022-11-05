868009ac1ea894941dad22504e6ca6e0e251ab92
Prashant <prashantstha1110@gmail.com>
2022-11-05 22:06:06 +0545

    solved update error in hostel change details

--- routes/api/addhostel.js
+++ routes/api/addhostel.js
@@ -8,7 +8,7 @@ const validateAddHostelInput = require('../../validation/addHostel');
 const Hostel = require('../../models/hostel');
 const hiddenHostel = require('../../models/hiddenHostel');
 
-/*Working: If _id is passed(in case of changing details), then on the basis availability it adds in 
+/*Working: If id(which is _id of hostel in hostel collection) is passed(in case of changing details), then on the basis availability it adds in 
 appropriate collection.If availability is no, it deletes from previous collection and adds in another collection. If _id is not present,it consider as new addition and checks availability for correct collection addition*/
 router.post('/addhostel',(req,res)=>{
 
@@ -25,7 +25,7 @@ router.post('/addhostel',(req,res)=>{
     }
 
     //if new hostel is added
-    if(req.body._id===undefined){
+    if(req.body.id===undefined){
         if(req.body.availability==="No"){
             const newHostel = new hiddenHostel({
                 imagepath : req.body.imagepath,
@@ -69,6 +69,7 @@ router.post('/addhostel',(req,res)=>{
         //if availability is no
         if(req.body.availability==="No"){
             const newHostel = new hiddenHostel({
+                id:req.body.id,
                 imagepath : req.body.imagepath,
                 title : req.body.title,
                 ownedby : req.body.ownedby,
@@ -83,7 +84,7 @@ router.post('/addhostel',(req,res)=>{
             });
             newHostel.save()
             .then(
-                Hostel.deleteOne({_id:req.body._id})
+                Hostel.deleteOne({_id:req.body.id})
                 .then(res.status(200).json("Hidden Successfully"))
                 .catch(res.status(500).json("Can not delete after saving changes in hostel collection"))
             )
@@ -91,7 +92,7 @@ router.post('/addhostel',(req,res)=>{
         }
         else{
             //changing details when availability is Yes
-            Hostel.updateOne({_id:req.body._id},{$set: {
+            Hostel.updateOne({_id:req.body.id},{$set: {
                 imagepath : req.body.imagepath,
                 title : req.body.title,
                 ownedby : req.body.ownedby,
@@ -105,7 +106,7 @@ router.post('/addhostel',(req,res)=>{
                 availability:req.body.availability
             }})
             .then(res.status(200).json("Sucessfully changed details"))
-            .catch(res.status(500).json("Could not change details"))
+            // .catch(res.status(500).json("Could not change details"))
         }
     }
 });
