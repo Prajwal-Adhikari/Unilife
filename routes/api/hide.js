9e99afd973eccd8b67a052abebe77b908ec78879
Prashant <prashantstha1110@gmail.com>
2022-11-05 22:13:11 +0545

    changed hiddenHostel model

---
+++ routes/api/hide.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const hiddenHostel = require('../../models/hostel');
+
+router.post('/hideHostel',(req,res)=>{
+
+})
+
+
+module.exports=router;
