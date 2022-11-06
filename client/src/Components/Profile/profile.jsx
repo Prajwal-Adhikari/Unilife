36a2aa6f0aebd15782d582d25e80a66807de2215
Prajwal-Adhikari <prazolppp@gmail.com>
2022-11-06 19:18:01 +0545

    Profile Page added

---
+++ client/src/Components/Profile/profile.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./profile.css"
+
+class Profile extends Component {
+    render() {
+            return (
+         <div>
+            <div className="profile_main">
+		<div class="profile_info">Pesronal Info</div>
+		<div class="profile_activity">Listing and purchases</div>
+        </div>
+	</div>
+    
+            )
+        } 
+    }
+
+
+export default Profile;
+
+
